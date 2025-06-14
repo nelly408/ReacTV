a063ecb6fb371dc9ea1de26f5800dbc241a84370
nelly408 <alexrnelson>
2025-06-14 16:47:24 -0700

    submission

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import { useState } from "react";
+import ShowDetails from "./shows/ShowDetails"
+import ShowSelection from "./shows/ShowSelection"
+import { tvShows } from "./shows/data";
+
 export default function App() {
+  const [shows, setShows] = useState(tvShows)
+  const [selectedShow, setSelectedShow] = useState()
   return (
     <>
       <header>
-        <p>React TV</p>
+       <ShowSelection shows = {shows} setSelectedShow={setSelectedShow}/>
       </header>
-      <main></main>
+      <main>
+        <ShowDetails key={selectedShow?.name} show ={selectedShow}/>
+        {}
+      </main>
     </>
   );
 }
--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,21 @@
 import "./shows.css";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails"
+import { useState } from "react";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({show}) {
+  const [selectedEps, setSelectedEps] = useState()
+  //JSX for return statement if there is no show
+  if(!show){
+    return(
+      <p>Please Select a show to view more details</p> 
+    )
+  }
+  //JSX id=f there is a show that renders the episodes for a show and the details for an episode
+  return (
+  <div className="show-details">
+      <EpisodeList name ={show.name} episodes={show.episodes} selectedEpisode ={selectedEps} setSelectedEpisode={setSelectedEps} />
+      <EpisodeDetails episode = {selectedEps}/>
+  </div>);
 }
